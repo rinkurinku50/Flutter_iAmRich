683c9488dd3a90e3a74607d88508bab240fd9b5c
Rinku Dhiman <rinkurinku50@gmail.com>
2020-05-06 22:15:55 +0530

    image changes

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,14 @@ import 'package:flutter/material.dart';
 // this is main function and its starting point
 void main() => runApp(
       MaterialApp(
-        home:Scaffold(
-        appBar: AppBar(
-          title: Text("Rinku"),
-          backgroundColor: Colors.blueGrey[900],
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Rinku"),
+            backgroundColor: Colors.blueGrey[900],
+          ),
+          body: Image(
+              image: AssetImage('images/imagenature.jpg')),
+          backgroundColor: Colors.blueGrey,
         ),
-       
-        ),
-      debugShowCheckedModeBanner: false,
       ),
     );
